12cd480ab4ed2df079a8a5c38333c3e7928caed5
yanagih <yanagih@jp.ibm.com>
2017-12-29 09:23:06 +0900

    makemecab.sh

--- makemecab.sh
+++ makemecab.sh
@@ -2,13 +2,15 @@
 
 PATH=$PATH:$HOME/bin:.
 
+echo $PATH
+
 tar xvzf mecab-0.996.tar.gz
 cd mecab-0.996
-./configure --prefix=/gpfs/fs01/user/s75b-8fd1c79afe3e99-4185b3aa7088/notebook/work --with-charset=utf8
+./configure --prefix=$HOME --with-charset=utf8
 make check
 make install
 
 tar xvzf mecab-ipadic-2.7.0-20070801.tar.gz
 cd mecab-ipadic-2.7.0-20070801
-./configure --prefix=/gpfs/fs01/user/s75b-8fd1c79afe3e99-4185b3aa7088/notebook/work --with-charset=utf8
+./configure --prefix=$HOME --with-charset=utf8
 make install
